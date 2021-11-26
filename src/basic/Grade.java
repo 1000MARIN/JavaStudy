8e1b0732d36810a20f53f06974a65fc3a8fd2343
1000marin <leexjin@gmail.com>
2021-11-26 10:17:28 +0900

    Create Grade.java

---
+++ src/basic/Grade.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Grade {
+
+	public static void main(String[] args) {
+		// 배열 생성
+		double[] grades = { 3.45, 2.82, 3.85, 3.94 };
+		
+		// 출력
+		System.out.printf("1학년: %.2f\n", grades[0]);
+		System.out.printf("2학년: %.2f\n", grades[1]);
+		System.out.printf("3학년: %.2f\n", grades[2]);
+		System.out.printf("4학년: %.2f\n", grades[3]);
+	} // end of main
+} // end of Grade
