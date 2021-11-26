849fc2a53eae04d8df27e61c347b19df5b4bb6c1
1000marin <leexjin@gmail.com>
2021-11-26 11:10:15 +0900

    Create ArrayLength.java

---
+++ src/basic/ArrayLength.java
@@ -0,0 +1,25 @@
+package basic;
+
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		// 변수 생성
+		String[] cores = {"자바", "자료구조", "알고리즘", "데이터베이스"};
+		String[] electives = {"컴퓨터 네트워크", "운영체제", "소프트웨어 공학"};
+		String[] generals = {"영어회화", "영독해", "영작문", "팀워크", "직업윤리"};
+		
+		// 핵심 과정 출력
+		System.out.printf("핵심과정 : ");
+		for (int i = 0; i < cores.length; i++) {
+			System.out.printf("%s ", cores[i]);
+		}
+		System.out.printf("\n선택과정 : ");
+		for (int i = 0; i < electives.length; i++) {
+			System.out.printf("%s ", electives[i]);
+		}
+		System.out.printf("\n교양과정 : ");
+		for (int i = 0; i < generals.length; i++) {
+			System.out.printf("%s ", generals[i]);
+		}
+	} // end of main
+} // end of ArrayLength
