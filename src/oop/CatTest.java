b8c5f4c3c8d1e4fcdad793b58a26a527d929cec5
1000marin <leexjin@gmail.com>
2021-11-30 09:52:16 +0900

    Update CatTest.java

--- src/oop/CatTest.java
+++ src/oop/CatTest.java
@@ -7,7 +7,7 @@ public class CatTest {
 		Cat c1 = new Cat();
 		Cat c2 = new Cat();
 		
-		// 객체의 필드 값을 변경하시오.
+		// 객체의 필드 값을 변경하시오. (초기화)
 		c1.name = "네로";
 		c2.name = "나비";
 		
@@ -20,9 +20,10 @@ public class CatTest {
 	} // end of main
 } // end of CatTest
 
+// 고양이 클래스
 class Cat {
 	
-	// 필드 영역
+	// 필드(상태) 영역
 	String name;	// 이름
 	String breed;	// 품종
 	int age;		// 나이
