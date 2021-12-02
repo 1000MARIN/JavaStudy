d042a65915e29efbfc070c57404862a5c92c2aef
1000marin <leexjin@gmail.com>
2021-12-02 10:20:48 +0900

    Update HeroTest.java

--- src/oop/HeroTest.java
+++ src/oop/HeroTest.java
@@ -4,16 +4,15 @@ public class HeroTest {
 
 	public static void main(String[] args) {
 		// 객체 생성
-		Hero ironMan = new Hero("아이언맨", 80);
-		Hero thanos	 = new Hero("타노스", 160);
 		Hero thor	 = new Hero("토르", 150);
-		Hero groot	 = new Hero("그루트", 80);
+		Hero thanos	 = new Hero("타노스", 160);
 		
-		// 모든 객체 정보를 출력
-		System.out.println(ironMan.toStr());
-		System.out.println(thanos.toStr());
-		System.out.println(thor.toStr());
-		System.out.println(groot.toStr());
+		// 토르의 펀치 -> 타노스
+		thor.punch(thanos);
+		
+		// 펀치 주고 받기
+		thanos.punch(thor);
+		thanos.punch(thor);
 
 	} // end of main
 } // end of HeroTest
@@ -31,7 +30,13 @@ class Hero {
 	}
 	
 	// 메소드
-	String toStr() {
-		return String.format("Hero { name: %s, hp: %d }", name, hp);
+	void punch(Hero enemy) {
+		// 때린 주체 객체
+		System.out.printf("[%s]의 펀치 !! ", name);
+		
+		// 맞은 객체에 대한 정보 출력
+		System.out.printf("%s의 HP: %d -> ", enemy.name, enemy.hp);
+		enemy.hp -= 10;
+		System.out.printf("%d\n", enemy.hp);
 	}
 } // end of Hero
