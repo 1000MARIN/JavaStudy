f366515f924f52381cda1e263ca714f33971c72e
1000marin <leexjin@gmail.com>
2021-12-07 17:29:37 +0900

    Update Starcraft.java

--- src/oop/Starcraft.java
+++ src/oop/Starcraft.java
@@ -7,15 +7,15 @@ public class Starcraft {
 		Marine ma = new Marine("레이너", 80);		// 생성자 호출 및 초기화
 		Medic me = new Medic("모랄레스", 60, 60);	// 생성자 호출 및 초기화
 		
-		// 마린의 스팀팩!
+		// 마린의 스팀팩 !
 		ma.stimpak();								// 메소드 호출
 		
-		// 메딕의 힘
+		// 메딕의 치유 !
 		me.heal(ma);								// 메소드 호출
 	} // end of main
 } // end of Stracraft 
 
-class Marine {										// 생성자 정의
+class Marine {
 	String name;
 	int hp;
 	
@@ -28,10 +28,11 @@ class Marine {										// 생성자 정의
 		System.out.printf("[%s]의 스팀팩 ! HP: %d -> ", name, hp);
 		hp -= 10;
 		System.out.printf("%d\n", hp);
+		
 	}
 } // end of Marine
 
-class Medic {										// 생성자 정의
+class Medic {
 	String name;
 	int hp;
 	int mp;
@@ -43,9 +44,8 @@ class Medic {										// 생성자 정의
 	}
 	
 	void heal(Marine target) {
-		System.out.printf("[%s]의 치유 ! -> [%s] hp(%d -> ", name, target.name, target.hp);
+		System.out.printf("[%s]의 치유 ! [%s] hp(%d -> ", name, target.name, target.hp);
 		target.hp += 10;
 		System.out.printf("%d)\n", target.hp);
-				
 	}
 } // end of Medic
